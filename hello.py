796683eeb660c724643c70206385b2ecc0b77a9f
matiasmunozs <chmunozs@gmail.com>
2023-03-07 23:24:17 -0300

    primer commit archivo

---
+++ hello.py
@@ -0,0 +1,19 @@
+#hola mundo
+
+print("Hola Python")
+print('Hola Python')
+
+"""
+comentario
+en varias lineas
+"""
+
+#type permite inferir el tipo de datos que tenemos
+print(type("Hola Python")) # tipo str
+print(type(5)) # tipo int ( integer)
+print(type(1.5)) #tipo de datos que no son enteros
+print(type(True)) # tipo de dato booleano
+print(type(5+1j)) # tipo de dato complejo
+
+
+
